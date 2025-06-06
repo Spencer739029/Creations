af0754c0da17a7ca4717bcd20565bfd3d3f1cc71
Spencer <spencertout@gmail.com>
2025-06-07 09:17:30 +1000

    Update working_number_sorter.py

--- working_number_sorter.py
+++ working_number_sorter.py
@@ -1,5 +1,8 @@
-import random
+# Uncomment the next line to use random numbers for testing
+# import random
 
+# Uncomment the next line to generate a list of random numbers
+# numbers = [random.randint(1, 100) for _ in range(10)]
 numbers = input("Enter a list of numbers separated by spaces: ").split()
 
 # Convert strings to integers (you can use float() if you want decimals)
