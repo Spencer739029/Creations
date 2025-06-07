e9a9806ff8115c3586acc7967257255de5d0975c
Spencer <spencertout@gmail.com>
2025-06-07 11:03:46 +1000

    Update display_window_message.py

--- display_window_message.py
+++ display_window_message.py
@@ -5,7 +5,7 @@ window = tk.Tk()
 window.title("My Message Window")
 window.geometry("300x150")  # Set window size (width x height)
 
-# Add a message label
+# Add a custom message label
 message = tk.Label(window, text="I love Summer \n so much!!! :)", font=("Arial", 20))
 message.pack(pady=30)  # Add some padding around the label
 
