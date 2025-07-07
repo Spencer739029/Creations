baea95b9d68d1bd18b6e909cb89d4799860661da
Spencer <spencertout@gmail.com>
2025-07-07 20:55:02 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,31 @@
-let items = [];
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
 
-const itemsDiv = document.getElementById("items")
-const input = document.getElementById("itemInput")
-const storageKey = "items";
+song.onloadedmetadata = function () {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+};
 
-function renderItems() {
-    itemsDiv.innerHTML = "";
-
-    for (const [idx, item] of Object.entries(items)) {
-        const container = document.createElement("div")
-        container.style.marginBottom = "10px"
-
-        const text = document.createElement("p")
-        text.style.display = "inline"
-        text.style.marginRight = "10px"
-        text.textContent = item;
-
-        const button = document.createElement("button")
-        button.textContent = "Delete"
-        button.onclick = () => removeItem(idx)
-
-        container.appendChild(text)
-        container.appendChild(button)
-
-        itemsDiv.appendChild(container)
+function playPause() {
+    if (ctrlIcon.classList.contains("fa-pause")) {
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add("fa-play");
+    } else {
+        song.play();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
     }
 }
 
-function loadItems() {
-    const oldItems = localStorage.getItem(storageKey)
-    if (oldItems) items = JSON.parse(oldItems)
-    renderItems()
-}
+// Start updating progress when the song plays
+song.addEventListener("play", () => {
+    setInterval(() => {
+        progress.value = song.currentTime;
+    }, 500);
+});
 
-function saveItem() {
-    const stringItems = JSON.stringify(items);
-    localStorage.setItem(storageKey, stringItems)
-}
-
-function addItem() {
-    const value = input.value;
-    if (!value) {
-        alert("You cannot add an empty item")
-        return
-    }
-    items.push("- " + value)
-    renderItems()
-    input.value = ""
-    saveItem()
-}
-
-function removeItem(idx) {
-    items.splice(idx, 1)
-    renderItems()
-    saveItem()
-}
-
-document.addEventListener("DOMContentLoaded", loadItems)
+progress.onchange = function () {
+    song.currentTime = progress.value;
+};
